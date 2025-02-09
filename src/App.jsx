01f958c7a3ce8958be198fc72463349e28e13b35
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-02-10 00:24:05 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Typography className='text-blue-400' variant='h1'>
+        Hello World
+      </Typography>
+    </>
+  )
+}
+
+export default App
